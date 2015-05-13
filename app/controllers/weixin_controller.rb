56faaf9f946380f7e26f339f89f7a26a8cb13032
Rwen <rwen@dowelltech.cn>
2015-05-13 15:37:56 +0800

    add weixin code
    add weixin code

--- app/controllers/weixin_controller.rb
+++ app/controllers/weixin_controller.rb
@@ -45,54 +45,48 @@ class WeixinController < ApplicationController
     
   def test
     
-    puts "aaaa"
-    puts root_url
-    if session[:openid]
-      
-      puts "bbbbb"
-      params = {
-        body: '11号公益圈订单',
-        out_trade_no: 'test005',
-        total_fee: 1,
-        spbill_create_ip: '127.0.0.1',
-        notify_url: root_url + 'weixin_custom/notify',
-        trade_type: 'JSAPI', # could be "JSAPI" or "NATIVE",
-        # openid: 'oaR9aswmRKvGhMdb6kJCgIFKBpeg' # required when trade_type is `JSAPI`
-        # openid: 'oaR9as940svyxuTEuKZgeibjC7ng'
-        openid: session[:openid]
+    params = {
+      body: '11号公益圈订单',
+      out_trade_no: 'test006',
+      total_fee: 1,
+      spbill_create_ip: '127.0.0.1',
+      notify_url: root_url + 'weixin_custom/notify',
+      trade_type: 'JSAPI', # could be "JSAPI" or "NATIVE",
+      openid: 'oaR9aswmRKvGhMdb6kJCgIFKBpeg' # required when trade_type is `JSAPI`
+      # openid: 'oaR9as940svyxuTEuKZgeibjC7ng'
+      # openid: session[:openid]
+    }
+
+    r = WxPay::Service.invoke_unifiedorder params
+    puts r
+
+    @weixin_init_success = false
+    if r["return_code"] == 'SUCCESS' && r["result_code"] == 'SUCCESS'
+
+      @js_noncestr = SecureRandom.uuid.tr('-', '')
+      @js_timestamp = Time.now.getutc.to_i.to_s
+      @app_id = r["appid"]
+      @package = "prepay_id=" + r["prepay_id"]
+
+      params_pre_pay_js = {
+        appId: @app_id,
+        nonceStr: @js_noncestr,
+        package: @package,
+        timeStamp: @js_timestamp,
+        signType: 'MD5'
       }
 
-      r = WxPay::Service.invoke_unifiedorder params
-      puts r
+      @js_pay_sign = WxPay::Sign.generate(params_pre_pay_js)
+      @weixin_init_success = true
+      puts @js_pay_sign
 
-      @weixin_init_success = false
-      if r["return_code"] == 'SUCCESS' && r["result_code"] == 'SUCCESS'
-
-        puts "aa"
-        @js_noncestr = SecureRandom.uuid.tr('-', '')
-        @js_timestamp = Time.now.getutc.to_i.to_s
-        @app_id = r["appid"]
-        @package = "prepay_id=" + r["prepay_id"]
-
-        params_pre_pay_js = {
-          appId: @app_id,
-          nonceStr: @js_noncestr,
-          package: @package,
-          timeStamp: @js_timestamp,
-          signType: 'MD5'
-        }
-
-        @js_pay_sign = WxPay::Sign.generate(params_pre_pay_js)
-        @weixin_init_success = true
-        puts @js_pay_sign
-
-      end
-      
-    else
-      
-      puts "ccccc"
-      
     end
+    
+    # if session[:openid]
+#
+#
+#
+#     end
 
   end
 
