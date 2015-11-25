de1f6f2ce42d4794849775934e96029c6aac4da1
446883859@qq.com <446883859@qq.com>
2015-11-25 15:17:19 +0800

    mxx  部分样式调整 服务协议新窗口打开                                                    无自定义图片高度问题
                                                       发起筹款第二步模板引用 列表自适应
    
     商品是否义卖不勾选造成商品的折扣为nil筹款团队商品列表报错问题201511251517

--- config/routes.rb
+++ config/routes.rb
@@ -236,5 +236,7 @@ Raisy::Application.routes.draw do
         get 'download' => 'weixin#download_file'
         
     end
-    
+    # make sure this rule is the last one
+    # match '*path' => proc { |env| Rails.env.development? ? (raise ActionController::RoutingError, %{No route matches "#{env["PATH_INFO"]}"}) : ApplicationController.action(:render_not_found).call(env) }
+    # get '*unmatched_route', :to => 'application#raise_not_found!'
 end
