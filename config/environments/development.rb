6964f32a05050b57bee8572216322b5b68afd1cb
jli <jli@github.com>
2015-12-08 10:19:40 +0800

    change error page config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Raisy::Application.configure do
     # This option may cause significant delays in view rendering with a large
     # number of complex assets.
     config.assets.debug = true
+    
+    # show error page set
+    config.error_page_ishow=false
 end
