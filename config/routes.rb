9aba3a211c6ebf47578b7ce31c4374c87722e692
jli <jli@github.com>
2015-12-09 10:37:11 +0800

    add mall home page

--- config/routes.rb
+++ config/routes.rb
@@ -63,6 +63,11 @@ Raisy::Application.routes.draw do
         get 'product_collections/:id', to: 'products#prod_collections', as: :product_collections
         
     end
+    
+    # Mall
+    scope 'mall' do
+      root 'mall#home', as: :mall_home
+    end
 
     resources :collections
     resources :organizations
@@ -245,5 +250,5 @@ Raisy::Application.routes.draw do
     #  get '*path' => proc { |env| Rails.env.development? ? (raise ActionController::RoutingError, %{No route matches "#{env["PATH_INFO"]}"}) : ApplicationController.action(:render_not_found).call(env) }
     # get '*unmatched_route', :to => 'application#raise_not_found!'
 
-     match '*path' => proc { |env| ApplicationController.action(:page_not_found).call(env) },via: [:get]
+    match '*path' => proc { |env| ApplicationController.action(:page_not_found).call(env) },via: [:get]
 end
