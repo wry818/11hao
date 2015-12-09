35e24efa75763f22abd5f0c91e150289c075cb12
446883859@qq.com <446883859@qq.com>
2015-12-09 16:24:49 +0800

    报表分页功能 loading  以及查询代码优化

--- app/controllers/admin/reports/campaign_visit_log_controller.rb
+++ app/controllers/admin/reports/campaign_visit_log_controller.rb
@@ -1,25 +1,55 @@
 class Admin::Reports::CampaignVisitLogController < Admin::Reports::ApplicationController
 
   def reportindex
-    sql_count="select count(1) as total FROM
-              \"public\".campaign_visit_logs
-              LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\""
-    sql="SELECT
-              campaign_visit_logs.\"id\",
-              campaign_visit_logs.campaign_id,
-              campaign_visit_logs.seller_id,
-              campaign_visit_logs.remote_ip,
-              campaign_visit_logs.visited_time,
-              campaign_visit_logs.nickname,
-              campaign_visit_logs.open_id,
-              campaigns.title
-              FROM
-              \"public\".campaign_visit_logs
-              LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\" "
-    sqlwhere=" where 1=1 and campaign_visit_logs.visited_time>='"+(Time.now-+ (60 * 60 * 24*7)).strftime("%Y-%m-%d")+"'"
-    sqlwhere+=" and campaign_visit_logs.visited_time<'"+Time.now.strftime("%Y-%m-%d")+"'"
-    @total=CampaignVisitLog.find_by_sql(sql_count+sqlwhere).first
-    @results=CampaignVisitLog.find_by_sql(sql+sqlwhere+" order by id desc")
+    # sql_count="select count(1) as total FROM
+    #           \"public\".campaign_visit_logs
+    #           LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\""
+    # sql="SELECT
+    #           campaign_visit_logs.\"id\",
+    #           campaign_visit_logs.campaign_id,
+    #           campaign_visit_logs.seller_id,
+    #           campaign_visit_logs.remote_ip,
+    #           campaign_visit_logs.visited_time,
+    #           campaign_visit_logs.nickname,
+    #           campaign_visit_logs.open_id,
+    #           campaigns.title
+    #           FROM
+    #           \"public\".campaign_visit_logs
+    #           LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\" "
+    # sqlwhere=" where 1=1 and campaign_visit_logs.visited_time>='"+(Time.now-+ (60 * 60 * 24*7)).strftime("%Y-%m-%d")+"'"
+    # sqlwhere+=" and campaign_visit_logs.visited_time<'"+Time.now.strftime("%Y-%m-%d")+"'"
+    # @total=CampaignVisitLog.find_by_sql(sql_count+sqlwhere).first
+    # @results=CampaignVisitLog.find_by_sql(sql+sqlwhere+" order by id desc")
+
+    logger.debug Time.now.to_s
+
+    if params[:from_date]
+      fromdate=Time.parse(params[:from_date]).strftime("%Y-%m-%d")
+    else
+      fromdate=(Time.now-+ (60 * 60 * 24*7)).strftime("%Y-%m-%d")
+    end
+
+    if params[:to_date]
+      endate=Time.parse(params[:to_date]).strftime("%Y-%m-%d")
+    else
+      endate= Time.now.strftime("%Y-%m-%d") +" 23:59:59"
+    end
+
+    @fromdate=fromdate
+    @todate=endate
+
+
+    @results=CampaignVisitLog.where("visited_time >= :start_date AND visited_time < :end_date",
+                                    {start_date:fromdate,
+                                     end_date:endate}).includes(:campaign).order(:id=>:desc).page(params[:page])
+
+    # logger.debug @results.to_yaml
+    # ids=@results.collect(&:campaign_id)
+    # @campagins=Campaign.all.where(id:ids)
+    @total=CampaignVisitLog.where("visited_time >= :start_date AND visited_time < :end_date",
+                                  {start_date:fromdate,
+                                   end_date:endate}).count
+
   end
 
   def reportsearch
@@ -28,32 +58,23 @@ class Admin::Reports::CampaignVisitLogController < Admin::Reports::ApplicationCo
     from_date=params[:from_date];
     to_date=params[:to_date];
 
-    logger.debug params.inspect
-    sql_count="select count(1) as total FROM
-              \"public\".campaign_visit_logs
-              LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\" where 1=1"
-    sql ="SELECT
-              campaign_visit_logs.\"id\",
-              campaign_visit_logs.campaign_id,
-              campaign_visit_logs.seller_id,
-              campaign_visit_logs.remote_ip,
-              campaign_visit_logs.visited_time,
-              campaign_visit_logs.nickname,
-              campaign_visit_logs.open_id,
-              campaigns.title
-              FROM
-              \"public\".campaign_visit_logs
-              LEFT JOIN campaigns on campaign_visit_logs.campaign_id=campaigns.\"id\" where 1=1 "
-    sqlwhere=""
-    if campagin_id.to_i>0
-      sqlwhere+=" and campaign_visit_logs.campaign_id="+campagin_id
+
+    from_date=Time.parse(from_date).strftime("%Y-%m-%d")
+    to_date=Time.parse(to_date).strftime("%Y-%m-%d")+" 23:59:59"
+    if campagin_id.to_i<1
+      @results=CampaignVisitLog.where("visited_time >= :start_date AND visited_time < :end_date",
+                                                                         {start_date:from_date,end_date: to_date}).includes(:campaign).order(:id=>:desc).page(params[:page])
+    else
+      @results=CampaignVisitLog.where(:campaign_id => campagin_id).where("visited_time >= :start_date AND visited_time < :end_date",
+                                                                         {start_date:from_date,end_date: to_date}).includes(:campaign).order(:id=>:desc).page(params[:page])
     end
-    sqlwhere+=" and campaign_visit_logs.visited_time>='"+from_date+"'"
-    sqlwhere+=" and campaign_visit_logs.visited_time<'"+to_date+"'"
 
-    @total=CampaignVisitLog.find_by_sql(sql_count+sqlwhere).first
-
-    @results=CampaignVisitLog.find_by_sql(sql+sqlwhere+" order by id desc")
+    # logger.debug @results.to_yaml ·
+    # ids=@results.collect(&:campaign_id)
+    # @campagins=Campaign.all.where(id:ids)
+    @total=CampaignVisitLog.where("visited_time >= :start_date AND visited_time < :end_date",
+                                  {start_date:from_date,
+                                   end_date: to_date}).count
 
     render :partial=>"report_content"
   end
