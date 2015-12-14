0cb05dc511569d45b24f59721deb6d907ed82572
mstarhand <446883859@qq.com>
2015-12-14 13:05:09 +0800

    商品标签添加

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,6 @@ class Tag < ActiveRecord::Base
     scope :isnot_destroy,->{where(:is_destroy => false)}
     scope :is_active,->{where(:active => true)}
 
-    has_many :product_tagses,class_name: "ProductTags",foreign_key: "tag_id"
+    has_many :product_tagses,class_name: "ProductTags",foreign_key: "tag_id", dependent: :destroy
     has_many :products,through: :product_tagses
 end
