a8637df78643a457b113ae17a38cbc8ecb0eacfa
jli <jli@github.com>
2015-12-21 17:22:55 +0800

    sav slider images

--- app/controllers/admin/mall_controller.rb
+++ app/controllers/admin/mall_controller.rb
@@ -3,7 +3,7 @@ class Admin::MallController < Admin::ApplicationController
     @root_categories = ProductCategory.showroot.order(:sort_mark)
     @all_products = Product.isnot_destroy.order(:id=>:desc)
     @top_categories = MallTopCategory.all
-    @slider_images = MallSliderImage.all
+    @slider_images = MallSliderImage.order(:sort_order).all
     @hot_products = MallHotProduct.all
   end
   
@@ -28,8 +28,22 @@ class Admin::MallController < Admin::ApplicationController
     
     if params[:hot_prod].present?
       params[:hot_prod].each_with_index { |hp, idx| 
-        MallHotProduct.create product_id: hp, sort_order: params["hot_prod_order_" + hp].to_i
+        MallHotProduct.create product_id: hp, sort_order: (params["hot_prod_order_" + hp] || "999").to_i
       }
+    end
+    
+    if params[:slider_images].present?
+      params[:slider_images].each do |si|
+        begin
+          img_hash = JSON.parse(si)
+          
+          MallSliderImage.create public_id: img_hash["public_id"], image_url: img_hash["secure_url"],
+            image_width: img_hash["image_width"], image_height: img_hash["image_height"], sort_order: img_hash["sort_order"],
+            crop_x: img_hash["crop_x"], crop_y: img_hash["crop_y"], crop_width: img_hash["crop_width"],
+            crop_height: img_hash["crop_height"], is_cropped: true
+        rescue
+        end
+      end
     end
     
     redirect_to admin_mall_settings_path and return
