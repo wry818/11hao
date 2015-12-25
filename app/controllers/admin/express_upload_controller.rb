4523fa1705a2e583b2bcfa373e6511be0b3e2b62
mstarhand <446883859@qq.com>
2015-12-25 18:16:41 +0800

    文件上传bug修复

--- app/controllers/admin/express_upload_controller.rb
+++ app/controllers/admin/express_upload_controller.rb
@@ -69,7 +69,7 @@ class Admin::ExpressUploadController < Admin::ApplicationController
     name= UUIDTools::UUID.timestamp_create.to_s.gsub('-', '')
     fullname= "/tmp/#{name}.xlsx"
 
-    if ((params[:upload]['datafile'].read.length)>(1024*1024*1))
+    if ((params[:upload]['datafile'].size)>(1024*1024*1))
       logger.debug "1001:"+params[:upload]['datafile'].read.length.to_s
       render :text => "max" and return
     end
