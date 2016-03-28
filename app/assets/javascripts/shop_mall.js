a54e1c067cd450d4890659fb2601008660bc7e4b
mstarhand <446883859@qq.com>
2016-03-28 14:44:28 +0800

    蜡笔义卖 小的修改

--- app/assets/javascripts/shop_mall.js
+++ app/assets/javascripts/shop_mall.js
@@ -55,7 +55,8 @@ window.shopmall = {
         var oUlBig = $oDiv.find(".oUlplay").get(0);
         var aBigLi = $(oUlBig).find('li');
         var oDivSmall = $oDiv.find(".smalltitle");
-
+        var $ulpoints=oDivSmall.find("ul");
+        $ulpoints.css("width",20*$ulpoints.find("li").length+"px");
         var aLiSmall = $(oDivSmall).find('li');
         var _this = this;
 
@@ -98,13 +99,120 @@ window.shopmall = {
         oNext.click(function () {
             __click();
         });
+
+
+        //$oDiv.swipe(function(){
+        // alert(0);
+        //});
+        //全局变量，触摸开始位置
+        var startX = 0, startY = 0;
+        var swipflag="";
+        //touchstart事件
+        function touchSatrtFunc(evt) {
+            try
+            {
+                //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+
+                var touch = evt.touches[0]; //获取第一个触点
+                var x = Number(touch.pageX); //页面触点X坐标
+                var y = Number(touch.pageY); //页面触点Y坐标
+                //记录触点初始位置
+                startX = x;
+                startY = y;
+
+            }
+            catch (e) {
+                alert('touchSatrtFunc：' + e.message);
+            }
+        }
+
+        //touchmove事件，这个事件无法获取坐标
+        function touchMoveFunc(evt) {
+            try
+            {
+                //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                var touch = evt.touches[0]; //获取第一个触点
+                var x = Number(touch.pageX); //页面触点X坐标
+                var y = Number(touch.pageY); //页面触点Y坐标
+
+                var text = 'TouchMove事件触发：（' + x + ', ' + y + '）';
+
+                //判断滑动方向
+                if (x - startX >10) {
+                    text ="right";
+                    clearInterval(timer)
+                    __click();
+                    timer = setInterval(__click, 3000)
+                    return false;
+                }
+                if (x - startX <-10) {
+                    text ="left";
+                    clearInterval(timer)
+                    _click();
+                    timer = setInterval(__click, 3000)
+                    return false;
+                }
+                swipflag=text;
+                //if (y - startY != 0) {
+                //    text += '<br/>上下滑动';
+                //}
+
+            }
+            catch (e) {
+                alert('touchMoveFunc：' + e.message);
+            }
+        }
+
+        //touchend事件
+        function touchEndFunc(evt) {
+            try {
+               // alert(swipflag);
+               // //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+               //
+               //if(swipflag=="left")
+               //{
+               //    _click();
+               //}
+               // if(swipflag=="right")
+               // {
+               //     __click();
+               // }
+               // alert(swipflag);
+            }
+            catch (e) {
+                alert('touchEndFunc：' + e.message);
+            }
+        }
+
+        //绑定事件
+        function bindEvent() {
+            $oDiv.get(0).addEventListener('touchstart', touchSatrtFunc, false);
+            $oDiv.get(0).addEventListener('touchmove', touchMoveFunc, false);
+            $oDiv.get(0).addEventListener('touchend', touchEndFunc, false);
+        }
+
+        //判断是否支持触摸事件
+        function isTouchDevice() {
+            try {
+                document.createEvent("TouchEvent");
+                //alert("支持TouchEvent事件！");
+
+                bindEvent(); //绑定事件
+            }
+            catch (e) {
+                //alert("不支持TouchEvent事件！" + e.message);
+                $oDiv.mouseover(function () {
+                    clearInterval(timer)
+                });
+                $oDiv.mouseout(function () {
+                    timer = setInterval(__click, 3000) //滚动间隔时间设置
+                });
+            }
+        }
+        isTouchDevice();
+
         var timer = setInterval(_click, 3000) //滚动间隔时间设置
-        $oDiv.mouseover(function () {
-            clearInterval(timer)
-        });
-        $oDiv.mouseout(function () {
-            timer = setInterval(__click, 3000) //滚动间隔时间设置
-        });
+
     },
 
     showbug: function () {
