a038a86b7f373b83a202f78b5f2c3e5802be8da7
mstarhand <446883859@qq.com>
2016-03-31 09:30:14 +0800

    自动关联Campaign  和产品页面

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325031956) do
+ActiveRecord::Schema.define(version: 20160330021941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -158,6 +158,7 @@ ActiveRecord::Schema.define(version: 20160325031956) do
     t.text      "minilogo"
     t.boolean   "is_featured"
     t.integer   "agreement_id"
+    t.integer   "product_id"
   end
 
   add_index "campaigns", ["entertainment_group_id"], name: "index_campaigns_on_entertainment_group_id", unique: true, using: :btree
