d9e2a645ef9fc2069aff5aeef492451aa5357eac
mstarhand <446883859@qq.com>
2016-04-12 14:33:56 +0800

    产品页面bug fix

--- app/assets/javascripts/shop_mall.js
+++ app/assets/javascripts/shop_mall.js
@@ -292,18 +292,57 @@ window.shopmall = {
             var weuiActionsheet = $('#weui_actionsheet');
             weuiActionsheet.addClass('weui_actionsheet_toggle');
             $("body").css("overflow","hidden");
-            $("body").bind("touchmove",function(event){event.preventDefault;//code});
+            var startY=0;
+            $("body").bind("touchstart",function(evt){
+                try
+                {
+                    //evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+
+                    var touch = evt.originalEvent.targetTouches[0];  //获取第一个触点
+                    var x = Number(touch.pageX); //页面触点X坐标
+                    var y = Number(touch.pageY); //页面触点Y坐标
+                    //记录触点初始位置
+
+                    startY = y;
+
+                }
+                catch (e) {
+                    alert('touchSatrtFunc：' + e.message);
+                }
+            })
+            $("body").bind("touchmove",function(evt){
+                try
+                {
+                    evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+                    var touch = evt.originalEvent.targetTouches[0];  //获取第一个触点
+                    var x = Number(touch.pageX); //页面触点X坐标
+                    var y = Number(touch.pageY); //页面触点Y坐标
+                    var scrolltoptemp=0;
+                    if (y - startY != 0) {
+                        var scrolltoptemp=(y - startY)/8;
+                        $(".js-options-select").scrollTop($(".js-options-select").scrollTop()+scrolltoptemp);
+                    }
+                    //console.log(y);
+                    console.log(scrolltoptemp);
+
+                }
+                catch (e) {
+                    alert('touchMoveFunc：' + e.message);
+                }
+            });
             //alert(1);
             mask.show().addClass('weui_fade_toggle').one('click', function () {
                 hideActionSheet(weuiActionsheet, mask);
                 $("body").css("overflow","auto");
                 $("body").unbind("touchmove");
+                $("body").unbind("touchstart");
                 //alert(2);
             });
             $('#actionsheet_cancel').one('click', function () {
                 hideActionSheet(weuiActionsheet, mask);
                 $("body").css("overflow","auto");
                 $("body").unbind("touchmove");
+                $("body").unbind("touchstart");
                 //alert(2);
             });
             weuiActionsheet.unbind('transitionend').unbind('webkitTransitionEnd');
