7e721098d0c9330a582dce7d2ce2e6508e132aae
mstarhand <446883859@qq.com>
2016-04-13 13:43:10 +0800

    bug test

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,10 +1,10 @@
 class ProductController < ApplicationController
   layout "shop_weixin"
   def index
-    # if params[":id"]
-    # else
-    #   params[":id"]="default"
-    # end
-    @campaign=Campaign.find_by_slug(params[":id"]);
+    if params[:id]
+    else
+      params[:id]="default"
+    end
+    @campaign=Campaign.find_by_slug(params[:id]);
   end
 end
