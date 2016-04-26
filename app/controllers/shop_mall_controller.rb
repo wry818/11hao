78fee1ca3156b67337290bab162ff4f7b9742a5f
mstarhand <446883859@qq.com>
2016-04-26 19:57:33 +0800

    bug test

--- app/controllers/shop_mall_controller.rb
+++ app/controllers/shop_mall_controller.rb
@@ -295,11 +295,11 @@ class ShopMallController < ApplicationController
     if params[:id]&&session[:openid]
       @party=Party.find(params[:id])
       if @party
-        loger.debug "1001"
+        loger.info "1001"
         weixin_get_user_info
-        loger.debug "1002"
+        loger.info "1002"
         @visit_log = ShareLog.new refid: @party.id, share_time: Time.now,type: 1
-        loger.debug "1003"
+        loger.info "1003"
         @visit_log.open_id = session[:openid]
         @visit_log.remote_ip = request.remote_ip
         @visit_log.nickname = @nickname
