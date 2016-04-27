538d8f69e8d5dc33446ac9945499d02f2154e0fc
mstarhand <446883859@qq.com>
2016-04-27 10:04:46 +0800

    bug test

--- app/assets/javascripts/shop_mall.js
+++ app/assets/javascripts/shop_mall.js
@@ -782,7 +782,8 @@ window.shopmall = {
                                 $(".js-showActionPary").unbind("click");
                                 $(".js-showActionPary").find("a").text("您已成功报名");
                                 $btn_submit1.removeClass("js-is-show-pay");
-                                location.replace(window.party.url);
+                                alert(window.party.url);
+                                location.href=window.party.url;
                             }
 
                         }
