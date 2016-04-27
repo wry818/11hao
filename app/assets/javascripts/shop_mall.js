9389489612f1b0486a7b2fd18381303f9c7a568f
mstarhand <446883859@qq.com>
2016-04-27 10:15:58 +0800

    bug test

--- app/assets/javascripts/shop_mall.js
+++ app/assets/javascripts/shop_mall.js
@@ -782,7 +782,7 @@ window.shopmall = {
                                 $(".js-showActionPary").unbind("click");
                                 $(".js-showActionPary").find("a").text("您已成功报名");
                                 $btn_submit1.removeClass("js-is-show-pay");
-                                alert(window.party.url);
+                                //alert(window.party.url);
                                 location.href=window.party.url;
                             }
 
@@ -865,7 +865,7 @@ window.shopmall = {
                     $(".js-showActionPary").unbind("click");
                     $(".js-showActionPary").find("a").text("您已成功报名");
                     $btn_submit1.removeClass("js-is-show-pay");
-                    location.replace(window.party.url);
+                    location.href=window.party.url;
                 },
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     //alert("抱歉，更新订单时出了问题，请联系我们帮您解决。");
